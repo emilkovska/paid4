38aec7a1a472a2ae059579b543149930a11096a2
emilkovska <milkovska.e@gmail.com>
2026-06-10 14:51:46 +0200

    Updated probDeath() where all die at 101

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,4 +1,4 @@
 test_that("Exclusion of diseases return the correct LHCE", {
   expect_equal(round(paid("Netherlands", related.diseases = c(14,25))[["lhce"]][1,1,"mean"]),
-               216498)
+               216449)
 })
